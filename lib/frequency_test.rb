09aeb4359078ce1285ed456c57abb0c347929640
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-04-24 16:13:26 -0700

    replaced word_frequency.rb with tested and passed frequency_test.rb, cleaned up code for word_frequency.rb, added additional verbage to word_frequency.erb

--- lib/frequency_test.rb
+++ lib/frequency_test.rb
@@ -3,19 +3,13 @@ class String
 		input_word = self
 		input_phrase = phrase
 		desired_word = []
-		split_phrase = [] #should be able to remove
 		split_phrase = input_phrase.split(" ")
 		frequency=0
 		split_phrase.each() do |word|
 			if input_word.==(word)
-				frequency+=1	
+				frequency+=1
 			end
 		end
-		#frequency = desired_word.length()
-		#frequency_final = frequency.to_s
-		#frequency = desired_word.length.to_s()
 		frequency
-
-
 	end
 end
--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -3,18 +3,13 @@ class String
 		input_word = self
 		input_phrase = phrase
 		desired_word = []
-		split_phrase = [] #should be able to remove
 		split_phrase = input_phrase.split(" ")
+		frequency=0
 		split_phrase.each() do |word|
 			if input_word.==(word)
-				desired_word.push(word)	
+				frequency+=1
 			end
 		end
-		#frequency = desired_word.length()
-		#frequency_final = frequency.to_s
-		frequency = desired_word.length.to_s()
 		frequency
-
-
 	end
 end
