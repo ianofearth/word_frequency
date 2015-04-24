0d6447f18868058e4a461f75c82eba7be0bfeb18
Ian Bevel <ianbevel@Ians-MacBook-Pro.local>
2015-04-24 10:34:46 -0700

    added second spec to word_frequency_spec.rb, rspec passed for all conditions

--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -5,4 +5,7 @@ describe('String#word_frequency') do
 	it('will take an input word and input phrase and determine the frequency of the word in the string') do
 		expect(("at").word_frequency("at")).to(eq("1"))
 	end
+	it('will take an input word and input phrase and will not return the phrase if it is an anagram of the inout word') do
+		expect(("at").word_frequency("ta")).to(eq("0"))
+	end
 end
